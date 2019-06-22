2ccfe26d7ffb9be0fb7495a65b1db55ccdc5fa52
Ratitigast <escrim.saiber@hotmail.com>
2019-06-22 02:11:25 -0500

    Imprime la lista de producciones, se espera identificar NT que generen T

---
+++ NT_Terminales.js
@@ -0,0 +1,7 @@
+var NT_Terminales = function(arrProd,cont){
+
+      var newP = document.createElement("P");
+       newP.innerHTML=arrProd[cont][0];
+       document.getElementById("demo3").appendChild(newP);
+
+}
